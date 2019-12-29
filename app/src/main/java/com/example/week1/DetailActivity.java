189bc820e0d2290f356064905588f4aa7806cb2e
KangsanKim07 <kksan07@kaist.ac.kr>
2019-12-29 12:37:37 +0900

    bigsize

--- app/src/main/java/com/example/week1/DetailActivity.java
+++ app/src/main/java/com/example/week1/DetailActivity.java
@@ -1,15 +1,25 @@
 package com.example.week1;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+
+import static java.security.AccessController.getContext;
 
 public class DetailActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -22,5 +32,7 @@ public class DetailActivity extends AppCompatActivity {
 
         ImageView imageView = (ImageView) findViewById(R.id.clicked_image);
         imageView.setImageURI(myuri);
+
     }
+
 }
